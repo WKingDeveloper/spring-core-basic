1ff7e6771df2b6444996a508c939baa8e9b6c192
WKingDeveloper <wkingdeveloper@gmail.com>
2022-03-03 21:24:27 +0900

    AppCofing를 통한 역할 분리 완료. DIP와 OCP를 지킬 수 있게 되었음.

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -2,15 +2,24 @@ package com.example.core.order;
 
 import com.example.core.discount.DiscountPoliy;
 import com.example.core.discount.FixDiscountPoliy;
+import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
+    private final MemberRepository memberRepository;
+    private final DiscountPoliy discountPoliy;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPoliy discountPoliy = new FixDiscountPoliy();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPoliy discountPoliy) {
+        this.memberRepository = memberRepository;
+        this.discountPoliy = discountPoliy;
+    }
+
+    //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPoliy discountPoliy = new FixDiscountPoliy();
+//    private final DiscountPoliy discountPoliy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
