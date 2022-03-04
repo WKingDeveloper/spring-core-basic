7def1b56a7961947c2fbc60a32dde4ee373fb6e4
WKingDeveloper <wkingdeveloper@gmail.com>
2022-03-04 09:05:30 +0900

    AppCofing에 스프링 Configuration과 Bean annotation을 설정하고 MemberApp 코드를 스프링 컨테이너를 통해 실행

--- src/main/java/com/example/core/AppConfig.java
+++ src/main/java/com/example/core/AppConfig.java
@@ -2,29 +2,38 @@ package com.example.core;
 
 import com.example.core.discount.DiscountPoliy;
 import com.example.core.discount.FixDiscountPoliy;
+import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
 import com.example.core.order.OrderService;
 import com.example.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPoliy());
     }
 
+    @Bean
     public DiscountPoliy discountPoliy() {
-        return new FixDiscountPoliy();
+        return new RateDiscountPolicy();
     }
 
 }
