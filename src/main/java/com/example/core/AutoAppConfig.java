5064f1e988fc39cf24e9b6a59a9310d4cc11cffb
KIM JUN SEONG <skoalp@frientrip.com>
2022-03-10 19:18:56 +0900

    동일한 이름의 bean이 자동 혹은 수동으로 생겼을 경우 테스트

--- src/main/java/com/example/core/AutoAppConfig.java
+++ src/main/java/com/example/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.core;
 
+import com.example.core.member.MemberRepository;
+import com.example.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- src/test/java/com/example/scan/AutoAppConfigTest.java
+++ src/test/java/com/example/scan/AutoAppConfigTest.java
@@ -3,7 +3,6 @@ package com.example.scan;
 import com.example.core.AutoAppConfig;
 import com.example.core.member.MemberService;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,8 +12,8 @@ public class AutoAppConfigTest {
     @Test
     void basicScan() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
-//
-//        MemberService memberService = ac.getBean(MemberService.class);
-//        assertThat(memberService).isInstanceOf(MemberService.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
